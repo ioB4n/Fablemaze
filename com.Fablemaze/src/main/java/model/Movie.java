36d902480ebd6887542dabc3e1ce58ee78634073
Ion Bogza <147531279+ioB4n@users.noreply.github.com>
2025-06-08 14:49:32 +0100

    Update Movie.java

--- com.Fablemaze/src/main/java/model/Movie.java
+++ com.Fablemaze/src/main/java/model/Movie.java
@@ -7,20 +7,28 @@ package model;
 public class Movie {
     private int movieId;
     private String title;
-    private Integer releaseYear; // Can be null
-    private Integer duration;    // Can be null
+    private Integer releaseYear; 
+    private Integer duration;
+    private String genres;
+    private String rating;
+    private Double imdbRating;
     private int sceneCount;
 
-    public Movie(int movieId, String title, Integer releaseYear, Integer duration, int sceneCount) {
+    public Movie(int movieId, String title, Integer releaseYear, Integer duration, String genres, 
+                 String rating, Double imdbRating, int sceneCount) {
         this.movieId = movieId;
         this.title = title;
         this.releaseYear = releaseYear;
         this.duration = duration;
+        this.genres = genres;
+        this.rating = rating;
+        this.imdbRating = imdbRating;
         this.sceneCount = sceneCount;
     }
 
-    public Movie(String title, Integer releaseYear, Integer duration, int sceneCount) {
-        this(0, title, releaseYear, duration, sceneCount);
+    public Movie(String title, Integer releaseYear, Integer duration, String genres, 
+                 String rating, Double imdbRating, int sceneCount) {
+        this(0, title, releaseYear, duration, genres, rating, imdbRating, sceneCount);
     }
 
     // Getters and setters
@@ -35,6 +43,15 @@ public class Movie {
 
     public Integer getDuration() { return duration; }
     public void setDuration(Integer duration) { this.duration = duration; }
+    
+    public String getGenres() { return genres; }
+    public void setGenres(String genres) { this.genres = genres; }
+
+    public String getRating() { return rating; }
+    public void setRating(String rating) { this.rating = rating; }
+
+    public Double GetImdbRating() { return imdbRating; }
+    public void setImdbRating(Double imdbRating) { this.imdbRating = imdbRating; }
 
     public int getSceneCount() { return sceneCount; }
     public void setSceneCount(int sceneCount) { this.sceneCount = sceneCount; }
